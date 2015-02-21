19856391f54e5da9730e58c5b21ba2265ea14ccd
Michael Turner <turnmichael@gmail.com>
2015-02-20 20:49:54 -0600

    Refactoring for expansion

--- src/main/java/org/shagie/util/mapserializer/Mapper.java
+++ src/main/java/org/shagie/util/mapserializer/Mapper.java
@@ -7,13 +7,25 @@ import java.lang.reflect.Field;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
+/**
+ * This class may be subclassed to allow for special handling of particular bits of
+ * functionality that occur during the process of converting the field to a map.
+ */
 public class Mapper {
     private static final Logger LOG = LoggerFactory.getLogger(Mapper.class);
 
+    /**
+     * Default constructor
+     */
     public Mapper() {
     }
 
-    public Map<String, Object> toMap(Object param) {
+    /**
+     * Convert the parameter (param) into a Map that represents it.
+     * @param param Object to be converted to a map
+     * @return Map of String (field name) to Object (field value)
+     */
+    public Map<String, Object> toMap(final Object param) {
         if (param == null) {
             return null;
         }
@@ -21,60 +33,105 @@ public class Mapper {
         Map<String, Object> retVal = new LinkedHashMap<String, Object>();
 
         for (Field f : param.getClass().getDeclaredFields()) {
-            if(f.getAnnotation(MapperIgnore.class) != null) { continue; }
-
-            final String name;
-            MapperRename renameAnno = f.getAnnotation(MapperRename.class);
-            if(renameAnno != null) {
-                name = renameAnno.value();
-            } else {
-                name = f.getName();
-            }
-
-            final boolean access = f.isAccessible();
+            if(ignore(f)) { continue; }
+            final String name = getName(f);
             if (f.getType().isPrimitive()) {
-                Object wrapped = null;
-                final Class<?> clazz = f.getType();
-
-                try {
-                    f.setAccessible(true);
-                    if (clazz.isInstance(boolean.class)) {
-                        wrapped = f.getBoolean(param);
-                    } else if (clazz.equals(byte.class)) {
-                        wrapped = f.getByte(param);
-                    } else if (clazz.equals(char.class)) {
-                        wrapped = f.getChar(param);
-                    } else if (clazz.equals(int.class)) {
-                        wrapped = f.getInt(param);
-                    } else if (clazz.equals(long.class)) {
-                        wrapped = f.getLong(param);
-                    } else if (clazz.equals(short.class)) {
-                        wrapped = f.getShort(param);
-                    } else if (clazz.equals(float.class)) {
-                        wrapped = f.getFloat(param);
-                    } else if (clazz.equals(double.class)) {
-                        wrapped = f.getDouble(param);
-                    }
-
-                    retVal.put(name, wrapped);
-                } catch (IllegalAccessException e) {
-                    LOG.error("Illegal access for field " + f.getName(), e);
-                } finally {
-                    f.setAccessible(access);
-                }
-
+                retVal.put(name, extractPrimitive(param, f));
             } else {
-                try {
-                    f.setAccessible(true);
-                    retVal.put(name, f.get(param));
-                } catch (IllegalAccessException e) {
-                    LOG.error("Illegal access for field " + f.getName(), e);
-                } finally {
-                    f.setAccessible(access);
-                }
+                Object value = extractObject(param, f);
+
+                // TODO transform?
+                // TODO null handle?
+
+                retVal.put(name, value);
             }
         }
 
         return retVal;
     }
+
+    /**
+     * Exgtract object associated with the field
+     * @param param object with the fields
+     * @param f field to be extracted
+     * @return value associated with the field
+     */
+    protected Object extractObject(Object param, Field f) {
+        Object value = null;
+        final boolean access = f.isAccessible();
+        try {
+            f.setAccessible(true);
+            value = f.get(param);
+        } catch (IllegalAccessException e) {
+            LOG.error("Illegal access for field " + f.getName(), e);
+        } finally {
+            f.setAccessible(access);
+        }
+        return value;
+    }
+
+    /**
+     * Extract a primitive field
+     * @param param object with the fields
+     * @param f field to be extracted
+     * @return Object of the type that wraps the primitive
+     */
+    protected Object extractPrimitive(Object param, Field f) {
+        Object wrapped = null;
+        final Class<?> clazz = f.getType();
+        final boolean access = f.isAccessible();
+
+        try {
+            f.setAccessible(true);
+            if (clazz.isInstance(boolean.class)) {
+                wrapped = f.getBoolean(param);
+            } else if (clazz.equals(byte.class)) {
+                wrapped = f.getByte(param);
+            } else if (clazz.equals(char.class)) {
+                wrapped = f.getChar(param);
+            } else if (clazz.equals(int.class)) {
+                wrapped = f.getInt(param);
+            } else if (clazz.equals(long.class)) {
+                wrapped = f.getLong(param);
+            } else if (clazz.equals(short.class)) {
+                wrapped = f.getShort(param);
+            } else if (clazz.equals(float.class)) {
+                wrapped = f.getFloat(param);
+            } else if (clazz.equals(double.class)) {
+                wrapped = f.getDouble(param);
+            }
+
+        } catch (IllegalAccessException e) {
+            LOG.error("Illegal access for field " + f.getName(), e);
+        } finally {
+            f.setAccessible(access);
+        }
+        return wrapped;
+    }
+
+    /**
+     * Given a field, return the name of the field.
+     * @param f field to be extracted
+     * @return String field name
+     */
+    protected String getName(final Field f) {
+        final MapperRename renameAnno = f.getAnnotation(MapperRename.class);
+        final String name;
+        if(renameAnno != null) {
+            name = renameAnno.value();
+        } else {
+            name = f.getName();
+        }
+        return name;
+    }
+
+    /**
+     * Determine if a given field should be ignored
+     * @param f field to be ignored (maybe)
+     * @return boolean ignore the field?
+     */
+    protected boolean ignore(final Field f) {
+        return f.getAnnotation(MapperIgnore.class) != null;
+    }
+
 }
